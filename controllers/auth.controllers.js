8d43e24b0dc32175d8d17f59807300f6c7fe20d3
DhimasRizaldy <dtekchannel02@gmail.com>
2024-09-17 13:28:50 +0700

    feat:login google fix

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -885,6 +885,124 @@ module.exports = {
   },
 
   // login with google
+  // loginGoogle: async (req, res, next) => {
+  //   try {
+  //     const { access_token } = req.body;
+
+  //     // Validasi access_token
+  //     if (!access_token) {
+  //       return res.status(400).json({
+  //         success: false,
+  //         message: "Access token required",
+  //         err: "Bad Request",
+  //         data: null,
+  //       });
+  //     }
+
+  //     // Menggunakan axios untuk mengambil data user dari Google API
+  //     const response = await axios.get(
+  //       `https://www.googleapis.com/oauth2/v3/userinfo?access_token=${access_token}`
+  //     );
+
+  //     // Validasi apakah respons Google berisi data yang dibutuhkan
+  //     const { email, name, picture, sub } = response.data;
+  //     if (!email || !name || !sub) {
+  //       return res.status(400).json({
+  //         success: false,
+  //         message: "Invalid Google account data",
+  //         err: "Google account is missing essential information",
+  //         data: null,
+  //       });
+  //     }
+
+  //     // Cari user berdasarkan email
+  //     let user = await prisma.users.findUnique({
+  //       where: {
+  //         email: email,
+  //       },
+  //       include: {
+  //         profiles: true,
+  //       },
+  //     });
+
+  //     // Jika user belum ada, buat user baru
+  //     if (!user) {
+  //       user = await prisma.users.upsert({
+  //         where: {
+  //           email: email,
+  //         },
+  //         update: {
+  //           googleId: sub,
+  //           profiles: {
+  //             update: {
+  //               imageProfile: picture,
+  //             },
+  //           },
+  //         },
+  //         create: {
+  //           username: name,
+  //           email: email,
+  //           googleId: sub,
+  //           isVerified: true,
+  //           profiles: {
+  //             create: {
+  //               fullName: name,
+  //               imageProfile: picture,
+  //             },
+  //           },
+  //         },
+  //       });
+  //     } else {
+  //       // Jika user sudah ada tetapi belum memiliki profile, buat profil baru
+  //       if (!user.profiles) {
+  //         await prisma.profiles.create({
+  //           data: {
+  //             userId: user.userId,
+  //             fullName: name,
+  //             imageProfile: picture,
+  //           },
+  //         });
+  //       }
+  //     }
+
+  //     // Hapus password dari objek user sebelum mengirim respons
+  //     if (user.password) {
+  //       delete user.password;
+  //     }
+
+  //     // Generate token JWT
+  //     let token = jwt.sign(
+  //       {
+  //         userId: user.userId,
+  //         username: user.username,
+  //         email: user.email,
+  //       },
+  //       process.env.JWT_SECRET,
+  //       { expiresIn: "1h" } // Token valid selama 1 jam
+  //     );
+
+  //     // Kirim respons sukses
+  //     return res.status(200).json({
+  //       success: true,
+  //       message: "Login successful",
+  //       err: null,
+  //       data: { ...user, token },
+  //     });
+  //   } catch (error) {
+  //     // Penanganan error, misalnya kesalahan saat komunikasi dengan Google API atau Prisma
+  //     if (error.response) {
+  //       return res.status(error.response.status).json({
+  //         success: false,
+  //         message: "Error fetching data from Google",
+  //         err: error.response.data,
+  //         data: null,
+  //       });
+  //     }
+
+  //     // Kesalahan umum lain
+  //     return next(error);
+  //   }
+  // },
   loginGoogle: async (req, res, next) => {
     try {
       const { access_token } = req.body;
@@ -916,7 +1034,7 @@ module.exports = {
       }
 
       // Cari user berdasarkan email
-      let user = await prisma.users.findUnique({
+      let user = await prismaClient.users.findUnique({
         where: {
           email: email,
         },
@@ -927,7 +1045,7 @@ module.exports = {
 
       // Jika user belum ada, buat user baru
       if (!user) {
-        user = await prisma.users.upsert({
+        user = await prismaClient.users.upsert({
           where: {
             email: email,
           },
@@ -955,7 +1073,7 @@ module.exports = {
       } else {
         // Jika user sudah ada tetapi belum memiliki profile, buat profil baru
         if (!user.profiles) {
-          await prisma.profiles.create({
+          await prismaClient.profiles.create({
             data: {
               userId: user.userId,
               fullName: name,
@@ -980,6 +1098,13 @@ module.exports = {
         process.env.JWT_SECRET,
         { expiresIn: "1h" } // Token valid selama 1 jam
       );
+
+      // Kirim token sebagai cookies
+      res.cookie("authToken", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+      });
 
       // Kirim respons sukses
       return res.status(200).json({
